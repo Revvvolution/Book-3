3efdcd2dcb5f1fd11be3d4b4644ddb5b23107fb1
Rev Huffman <148085981+Revvvolution@users.noreply.github.com>
2024-04-25 15:46:08 -0400

    Finished Deshawn's Dog Walking - all embedded dataset functions working

--- dog-walking/scripts/Walkers.js
+++ dog-walking/scripts/Walkers.js
@@ -2,12 +2,27 @@ import { getWalkers } from "./database.js"
 
 const walkers = getWalkers()
 
+document.addEventListener(
+    "click",
+    (theClickEventTaco) => {
+        const whatWasClickedOnTaco = theClickEventTaco.target
+
+        if (whatWasClickedOnTaco.dataset.type === "walkerTaco") {
+            window.alert(`This walker works in ${whatWasClickedOnTaco.dataset.city}`)
+        }
+        
+    }
+)
+
 
 export const Walkers = () => {
     let walkerHTML = "<ul>"
 
     for (const walker of walkers) {
-        walkerHTML += `<li>${walker.name}</li>`
+        walkerHTML += `<li data-id="${walker.id}"
+                           data-city="${walker.city}"
+                           data-type="walkerTaco"            
+                           >${walker.name}</li>`
     }
 
     walkerHTML += "</ul>"
