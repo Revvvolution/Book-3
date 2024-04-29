1289610c09e6adc1a9d396103549fd1e1ea53966
Rev Huffman <148085981+Revvvolution@users.noreply.github.com>
2024-04-29 12:15:31 -0400

    Added clickEvent for Shipping Ships to display hauler it is on

--- shippingShipShips/scripts/shippingShipList.js
+++ shippingShipShips/scripts/shippingShipList.js
@@ -1,4 +1,36 @@
-import { getShippingShips } from "./database.js";
+import { getShippingShips, getHaulingShips } from "./database.js";
+
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+
+        // Was a shipping ship list item clicked?
+        if (itemClicked.dataset.type === "shippingShip") {
+
+            // Get the haulerId value of the shipping ship clicked
+            const haulerForShip = itemClicked.dataset.hauledby
+
+            // Define a default object for the found hauler
+            let haulingShip = { name: "Incorrect" }
+
+            // Iterate the array of hauler objects
+            const haulers = getHaulingShips()
+
+            for (const hauler of haulers) {
+                // Does the haulerId foreign key match the id of the current hauler?
+                if (parseInt(haulerForShip) === hauler.id) {
+                    // Reassign the value of `haulingShip` to the current hauler
+                    haulingShip = hauler.name
+                }
+            }
+            // Show an alert to the user with this format...
+            // Palais Royal is being hauled by Seawise Giant
+            window.alert(`${itemClicked.dataset.name} is being hauled by ${haulingShip}`)
+        }
+    }
+)
 
 
 export const ShippingShipList = () => {
@@ -8,7 +40,9 @@ export const ShippingShipList = () => {
 
     for (const ship of shippingShips) {
         shippingShipHTML += `<li data-id="${ship.id}"
-                                 data-type="shippingShip">
+                                 data-type="shippingShip"
+                                 data-hauledby="${ship.haulerId}"
+                                 data-name="${ship.name}">
                                 ${ship.name}
                                 </li>`
     }
