cedc409eace1bd8934b4151caef781a2bdab16e2
Rev Huffman <148085981+Revvvolution@users.noreply.github.com>
2024-05-01 13:05:54 -0400

    Added eventListener for Employee list for items sold window alert

--- brewed-awakenings/scripts/Employees.js
+++ brewed-awakenings/scripts/Employees.js
@@ -1,12 +1,48 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
 
 const employees = getEmployees()
+const orders = getOrders()
+
+
+const employeeOrders = (id) => {
+    let fulfilledOrders = 0
+
+    for (const order of orders) {
+        if (order.employeeId === parseInt(id)) {
+            fulfilledOrders += 1
+        }
+    }
+
+    return fulfilledOrders
+}
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.dataset.type === "employee") {
+
+            const employeeId = itemClicked.dataset.id
+
+            const orderCount = employeeOrders(employeeId)
+
+
+            window.alert(` ${itemClicked.dataset.name} sold ${orderCount} products `)
+            
+            }
+        }
+)
+
 
 export const Employees = () => {
     let html = "<ul class='listFont'>"
 
     for (const employee of employees) {
-        html += `<li>${employee.name}</li>`
+        html += `<li data-type="employee"
+                     data-id="${employee.id}"
+                     data-name="${employee.name}">
+                    ${employee.name}
+                    </li>`
     }
 
     html += "</ul>"
